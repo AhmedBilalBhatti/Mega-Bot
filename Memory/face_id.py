dbf019608cfd313c6a62503d5217ce1c8ddc788b
AhmedBilalSSG <110194946+AhmedBilalSSG@users.noreply.github.com>
2024-03-13 21:47:03 +0500

    commit

--- Memory/face_id.py
+++ Memory/face_id.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import time
 import numpy as np
 from PIL import Image
 from MegaBot.settings import BASE_DIR
@@ -59,57 +60,50 @@ class FaceRecognition:
         recognizer.save(str(BASE_DIR) +'/Face_trainer/trainer.yml')
         print("\n {0} faces trained. Exiting Program".format(len(np.unique(ids))))
         
-        
-    def recognizeFace(self):
-        recognizer.read(str(BASE_DIR) +'/Face_trainer/trainer.yml')
-        cascadePath = str(BASE_DIR) +'/haarcascade_frontalface_default.xml'
-        faceCascade = cv2.CascadeClassifier(cascadePath)
 
-        font = cv2.FONT_HERSHEY_SIMPLEX
+def recognizeFace(self):
+    recognizer.read(str(BASE_DIR) +'/Face_trainer/trainer.yml')
+    cascadePath = str(BASE_DIR) +'/haarcascade_frontalface_default.xml'
+    faceCascade = cv2.CascadeClassifier(cascadePath)
 
-        confidence = 0
-        cam = cv2.VideoCapture(0)
+    font = cv2.FONT_HERSHEY_SIMPLEX
 
-        minW = 0.1*cam.get(3)
-        minH = 0.1*cam.get(4)
+    confidence = 0
+    cam = cv2.VideoCapture(0)
 
-        while True:
+    minW = 0.1*cam.get(3)
+    minH = 0.1*cam.get(4)
 
-            ret, img =cam.read()
+    start_time = time.time()
+    while True:
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(int(minW), int(minH)))
 
-            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-            faces = faceCascade.detectMultiScale( 
-                gray,
-                scaleFactor = 1.2,
-                minNeighbors = 5,
-                minSize = (int(minW), int(minH)),
-            )
-
-            for(x,y,w,h) in faces:
-
-                cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
-
-                face_id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
-
-                if (confidence < 100):
-                    name = 'Detected'
-                else:
-                    name = "Unknown"
-                
-                cv2.putText(img, str(name), (x+5,y-5), font, 1, (255,255,255), 2)
-                cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+        for (x,y,w,h) in faces:
+            cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+            face_id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+            if (confidence < 100):
+                name = 'Detected'
+            else:
+                name = "Unknown"
             
-            cv2.imshow('Detect Face',img) 
+            cv2.putText(img, str(name), (x+5,y-5), font, 1, (255,255,255), 2)
+            cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+        
+        cv2.imshow('Detect Face', img)
 
-            k = cv2.waitKey(10) & 0xff 
-            if k == 27:
-                break
-            if confidence > 50:
-                break
+        if time.time() - start_time > 10:
+            break
 
-        print("\n Exiting Program")
-        cam.release()
-        cv2.destroyAllWindows()
-        print(face_id)
-        return face_id
+        k = cv2.waitKey(10) & 0xff 
+        if k == 27:
+            break
+        if confidence > 50:
+            break
+
+    print("\n Exiting Program")
+    cam.release()
+    cv2.destroyAllWindows()
+    print(face_id)
+    return face_id
