78972efb21cec584d77ac8692476ac023dad0418
AhmedBilalSSG <110194946+AhmedBilalSSG@users.noreply.github.com>
2024-03-13 23:37:19 +0500

    commit

--- Memory/Emails.py
+++ Memory/Emails.py
@@ -8,7 +8,7 @@ def Signup_Thanks(name, email, message):
     email_context = {'name': name, 'email': email, 'message': message}
     html_message = render_to_string('Thanks.html', email_context)
     recipient_list = [email]
-    
+    print(email_context)
     try:
         send_mail(subject, '', 'ahmadbilalssg@gmail.com', recipient_list, html_message=html_message, fail_silently=True)
         return True, 'Your message has been submitted successfully.'
