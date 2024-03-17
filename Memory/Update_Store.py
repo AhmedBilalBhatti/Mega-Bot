1c3b198c1da6f5f76eb7a8dbb925814a6f2fd263
AhmedBilalSSG <110194946+AhmedBilalSSG@users.noreply.github.com>
2024-03-18 00:21:59 +0500

    commit

--- Memory/Update_Store.py
+++ Memory/Update_Store.py
@@ -5,6 +5,41 @@ from django.core.files.storage import default_storage
 from .models import *
 import os
 
+# def upload_profile_pic(request):
+#     session = request.session.get('user_id')
+#     if request.method == 'POST' and request.FILES.get('profile_picture'):
+#         profile_picture = request.FILES['profile_picture']
+
+#         try:
+#             user = Signups.nodes.get(uid=session)
+#             file_extension = os.path.splitext(profile_picture.name)[1]
+#             new_file_name = f"profile_{session}{file_extension}"
+
+#             if user.profile_image:
+#                 try:
+#                     default_storage.delete(user.profile_image)
+#                 except FileNotFoundError:
+#                     pass
+
+#             # Define the file path within the media directory
+#             file_path = os.path.join('Profile', new_file_name)
+
+#             # Save the file to the media directory
+#             with default_storage.open(file_path, 'wb') as f:
+#                 for chunk in profile_picture.chunks():
+#                     f.write(chunk)
+
+#             # Update the user's profile_image property with the correct URL format
+#             user.profile_image = os.path.join(settings.MEDIA_URL, file_path.replace('\\', '/'))
+#             user.save()
+
+#             # Redirect to the chat page after successful upload
+#             return redirect('chat')
+#         except Signups.DoesNotExist:
+#             return HttpResponseNotFound('User not found.')
+
+#     return HttpResponse('No file selected or invalid request.')
+
 def upload_profile_pic(request):
     session = request.session.get('user_id')
     if request.method == 'POST' and request.FILES.get('profile_picture'):
@@ -15,6 +50,7 @@ def upload_profile_pic(request):
             file_extension = os.path.splitext(profile_picture.name)[1]
             new_file_name = f"profile_{session}{file_extension}"
 
+            # Delete the previous profile image if it exists
             if user.profile_image:
                 try:
                     default_storage.delete(user.profile_image)
@@ -31,6 +67,8 @@ def upload_profile_pic(request):
 
             # Update the user's profile_image property with the correct URL format
             user.profile_image = os.path.join(settings.MEDIA_URL, file_path.replace('\\', '/'))
+
+            # Save the changes to the user node
             user.save()
 
             # Redirect to the chat page after successful upload
