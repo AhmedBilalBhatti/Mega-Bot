ddbe865dd2e858c23ebfce2e9a57196a8bb3d906
AhmedBilalSSG <110194946+AhmedBilalSSG@users.noreply.github.com>
2024-03-31 01:08:20 +0500

    commit

--- Memory/prolog.py
+++ Memory/prolog.py
@@ -1,35 +1,36 @@
 from django.http import JsonResponse
+from django.conf import settings
 import pytholog as pl
+from .views import *
+import os
 
 def prolog_handling(request):
+    session = request.session.get('user_id')
     if request.method == 'POST' and request.FILES.get('prolog_file'):
         prolog_file = request.FILES['prolog_file']
         if prolog_file:
-            print(prolog_file)
-            # prolog_contents = prolog_file.read().decode('utf-8')
-            # print(prolog_contents)
+            prolog_contents = prolog_file.read().decode('utf-8')
+            print(prolog_contents)
 
+            media_folder = settings.MEDIA_ROOT
+            temp_file_path = os.path.join(media_folder, f"prolog/temp_prolog_file_{session}.pl")
 
+            if os.path.exists(temp_file_path):
+                os.remove(temp_file_path)
 
-
+            with open(temp_file_path, 'w') as temp_file:
+                temp_file.write(prolog_contents)
 
             new_kb = pl.KnowledgeBase("family")
             new_kb.clear_cache()
-            path = "prolog_file"
-            new_kb.from_file(path)
-            print(new_kb.query(pl.Expr("Parent(Ahmed,Ali)"))[0])
-            print(new_kb.query(pl.Expr("Child(Alia, Nadia)"))[0])
+            new_kb.from_file(temp_file_path)
 
+            parent_query_result = new_kb.query(pl.Expr("Parent(Ahmed,Ali)"))
+            child_query_result = new_kb.query(pl.Expr("Child(Alia, Nadia)"))
 
+            print(parent_query_result[0] if parent_query_result else "No result")
+            print(child_query_result[0] if child_query_result else "No result")
 
-
-            
             return JsonResponse({'bot_response': 'This is a Prolog file I have read. What do you want to know?'})
-    
+
     return JsonResponse({'bot_response': 'No file received.'})
-
-
-
-
-
-
---
+++ media/prolog/temp_prolog_file.pl
@@ -0,0 +1,15 @@
+Male(Ali).
+Male(Ahmed). 
+Female(Alia).
+Female(Nadia).
+
+Parent(Ahmed, Ali).
+Parent(Ali, Alia).
+Parent(Nadia, Alia).
+
+Child(Ali, Ahmed).
+Child(Alia, Ali).
+Child(Alia, Nadia).
+
+Father(X,Y) :- Male(X), Parent(X,Z), Child(Z,Y).
+Mother(X,Y) :- Female(X), Parent(X,Z), Child(Z,Y).
