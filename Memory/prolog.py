d9bce298573f3b42f09c2c91dbd54a6d78ebe363
AhmedBilalSSG <110194946+AhmedBilalSSG@users.noreply.github.com>
2024-03-31 00:53:28 +0500

    commit

--- Memory/prolog.py
+++ Memory/prolog.py
@@ -1,9 +1,35 @@
 from django.http import JsonResponse
+import pytholog as pl
 
 def prolog_handling(request):
     if request.method == 'POST' and request.FILES.get('prolog_file'):
         prolog_file = request.FILES['prolog_file']
         if prolog_file:
-            print(f"Received file: {prolog_file.name}")
+            print(prolog_file)
+            # prolog_contents = prolog_file.read().decode('utf-8')
+            # print(prolog_contents)
+
+
+
+
+
+            new_kb = pl.KnowledgeBase("family")
+            new_kb.clear_cache()
+            path = "prolog_file"
+            new_kb.from_file(path)
+            print(new_kb.query(pl.Expr("Parent(Ahmed,Ali)"))[0])
+            print(new_kb.query(pl.Expr("Child(Alia, Nadia)"))[0])
+
+
+
+
+            
             return JsonResponse({'bot_response': 'This is a Prolog file I have read. What do you want to know?'})
+    
     return JsonResponse({'bot_response': 'No file received.'})
+
+
+
+
+
+
