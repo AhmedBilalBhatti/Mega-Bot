e284221b4a352f04f7d8ecb0d02c654c07d1bf93
AhmedBilalSSG <ahmadbilalssg@gmail.com>
2024-05-06 01:27:22 +0500

    commit

--- Memory/views.py
+++ Memory/views.py
@@ -203,7 +203,10 @@ def chat(request):
                 vect_features = result[1]
                 check = lemmatized_tokens[0]
                 person = detect_persons(lemmatized_tokens)
-                if person:
+                che = person[0][0]
+                ps = Prolog_Members.nodes.first(full_name=che)
+                again_check = True if ps else False
+                if person and again_check:
                     Total_person = len(person)
                     if Total_person == 1:
                         name = "".join(lemmatized_tokens[1])
@@ -216,7 +219,6 @@ def chat(request):
                                         MATCH (p)-[r:`{relation}`]-(other)
                                         RETURN other.full_name; """
                         results, meta = db.cypher_query(cypher_query, params)
-                        # mem = results[0][0]
                         formatted_names = []
                         for result in results:
                             other_name = result[0]
@@ -231,12 +233,12 @@ def chat(request):
                         bot_response = f"{name_str.capitalize()} is {relation} of {name.capitalize()}."
                         maintain_history(request, message, bot_response)
                         return JsonResponse({'bot_response': bot_response})
-
-                    # elif Total_person == 2:
-
-
-                    # else:
-
+                else:
+                    bot_response = kernel.respond(message)
+                    if bot_response == "I'm sorry, I didn't understand what you said.":
+                        bot_response = web_scraping(message)
+                        maintain_history(request, message, bot_response)
+                        return JsonResponse({'bot_response': bot_response})
             else:
                 bot_response = kernel.respond(message)
                 if bot_response == "I'm sorry, I didn't understand what you said.":
